0765e442db132df4bbe95e4f5e7324ea65eb1967
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2022-02-03 16:57:00 -0500

    Update enchant_detection.js
    1: Added a lot of stuff you accidentally deleted when pushing it
    2: Removed debugging lines that were not supposed to be there
    3: Made it look cleaner/easier to read

--- scripts/modules/enchant_detection.js
+++ scripts/modules/enchant_detection.js
@@ -1,33 +1,42 @@
 // @baboonie#2522
-import { world, MinecraftEnchantmentTypes, ItemStack, MinecraftItemTypes } from "mojang-minecraft"
+import {
+	world,
+	MinecraftEnchantmentTypes,
+	ItemStack,
+	MinecraftItemTypes
+} from "mojang-minecraft"
 
 const Enchant_Detection = () => {
-    let allEncants = Object.keys(MinecraftEnchantmentTypes)
+	let allEnchants = Object.keys(MinecraftEnchantmentTypes)
 
-    world.events.tick.subscribe(tick => {
-        const players = [...world.getPlayers()]
+	world.events.tick.subscribe(tick => {
+		const players = [...world.getPlayers()]
 
-        players.forEach(player => {
-            const inventory = player.getComponent('minecraft:inventory').container
+		players.forEach(player => {
+			const inventory = player.getComponent('minecraft:inventory').container
 
-            for (let i = 0; i < inventory.size; i++) {
-                const item = inventory.getItem(i)
-                if (item) {
-                    const itemEnchantments = item.getComponent('minecraft:enchantments').enchantments
+			for (let i = 0; i < inventory.size; i++) {
+				const item = inventory.getItem(i)
+				if (!item) continue
 
-                        if (!itemEnchantments.hasEnchantment(MinecraftEnchantmentTypes[enchantmentKey])) continue
-                        const enchantmentData = itemEnchantments.getEnchantment(MinecraftEnchantmentTypes[enchantmentKey])
-                        console.warn((enchantmentData.level > enchantmentData.type.maxLevel))
-                        if (enchantmentData.level > enchantmentData.type.maxLevel) {
-                            itemEnchantments.removeEnchantment(MinecraftEnchantmentTypes[enchantmentKey])
-                            inventory.setItem(i, new ItemStack(MinecraftItemTypes['air'], 1, 0))
-                            player.triggerEvent('hydra:kick')
-                        }
-                    }
-                }
-            }
-        })
-    })
+				const itemEnchantments = item.getComponent('minecraft:enchantments').enchantments
+				for (const enchantmentKey of allEnchants) {
+					const enchantment = MinecraftEnchantmentTypes[enchantmentKey]
+					if (!itemEnchantments.hasEnchantment(enchantment)) continue
+
+					const enchantmentData = itemEnchantments.getEnchantment(enchantment)
+					if (!enchantmentData.level > enchantmentData.type.maxLevel) continue
+
+					itemEnchantments.removeEnchantment(enchantment)
+					inventory.setItem(i, new ItemStack(MinecraftItemTypes['air'], 1, 0))
+					player.triggerEvent('hydra:kick')
+
+				}
+			}
+		})
+	})
 }
 
-export { Enchant_Detection }
+export {
+	Enchant_Detection
+}
