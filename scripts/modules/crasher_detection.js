d7e433a2200a232d69ba968bef028747a4b076d6
RomanMender <discojamesw@gmail.com>
2022-02-16 10:35:48 +0000

    Create crasher_detection.js

---
+++ scripts/modules/crasher_detection.js
@@ -0,0 +1,18 @@
+import { world } from "mojang-minecraft"
+
+const Crasher_Detection = () => {
+    world.events.tick.subscribe(tick => {
+        const players = [...world.getPlayers()]
+
+        players.forEach(player => {
+
+            const { x, y, z } = player.location
+
+            if (Math.abs(x) > 30000000 || Math.abs(y) > 30000000 || Math.abs(z) > 30000000) {
+                player.triggerEvent('hydra:kick')
+            }
+        })
+    })
+}
+
+export { Crasher_Detection }
