690adfa9fec43397a198ee6a85eaafedf435d966
FeedFall8 <58020585+FeedFall8@users.noreply.github.com>
2022-03-10 10:24:19 -0600

    Create nuker_detection.js

---
+++ scripts/modules/nuker_detection.js
@@ -0,0 +1,26 @@
+import { world, BlockLocation } from "mojang-minecraft"
+
+var BlockLog = new Map()
+
+const Nuker_Detection = () => {
+    world.events.blockBreak.subscribe(use => {
+
+        if (BlockLog.has(use.player.nameTag)) { var logs = BlockLog.get(use.player.nameTag) } else { var logs = [] }
+
+        logs.push(new Date())
+
+        var filtered = logs.filter(time => time.getTime() > new Date().getTime() - 100)
+        BlockLog.set(use.player.nameTag, filtered)
+
+        if (filtered.length > 2) {
+            var permutation = use.brokenBlockPermutation.clone()
+            const { x, y, z } = use.block
+            const block = world.getDimension('overworld').getBlock(new BlockLocation(x, y, z))
+            block.setPermutation(permutation)
+            try { use.player.runCommand('kill @e[x=' + x + ',y=' + y + ',z=' + z + ',r=10,c=1,type=item]') } catch (e) { }
+        }
+
+    })
+}
+
+export { Nuker_Detection }
