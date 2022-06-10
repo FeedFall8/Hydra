1b5103cb2f0b73f43225bbce609da36414f1e7ad
FeedFall8 <58020585+FeedFall8@users.noreply.github.com>
2022-06-10 15:36:27 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,6 @@ SpamDetection()
 /* Nuker Detection */
 import { Nuker_Detection } from './modules/nuker_detection.js'
 Nuker_Detection()
+/*Item Place Detection/Block Detection */
+import { Item_Detection} from './modules/item_place_detection.js'
+Item_Detection()
